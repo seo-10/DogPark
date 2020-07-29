49b88818d077d50bcb5145765de4919fe80aa665
Wonyoung Seo <swy1487@naver.com>
2020-07-29 19:32:24 +0900

    90%commit wonyoung

--- dogpark_project/src/com/dogpark/service/BoardListService.java
+++ dogpark_project/src/com/dogpark/service/BoardListService.java
@@ -2,11 +2,10 @@ package com.dogpark.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-
-
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.dogpark.dao.Dao;
 import com.dogpark.dto.BoardBean;
 
