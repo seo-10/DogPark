ff3f8ec9d6d9d09de0191025c1ae7205cec3d1da
Suhwan Yu <wapet88@gmail.com>
2020-07-29 16:15:23 +0900

    Dto getter/setter add

--- dogpark_project/src/com/dogpark/action/SignProAction.java
+++ dogpark_project/src/com/dogpark/action/SignProAction.java
@@ -1,5 +1,47 @@
 package com.dogpark.action;
 
-public class SignProAction {
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dogpark.dto.ActionForward;
+import com.dogpark.dto.Dto;
+
+public class SignProAction implements Action {
 	int a = 1;
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		ActionForward forward=null;
+		Dto dto = null;
+		
+		ServletContext context = request.getServletContext();
+
+		dto = new Dto();
+		dto.setBOARD_NAME(multi.getParameter("BOARD_NAME"));
+		dto.setBOARD_PASS(multi.getParameter("BOARD_PASS"));
+		dto.setBOARD_SUBJECT(multi.getParameter("BOARD_SUBJECT"));
+		dto.setBOARD_CONTENT(multi.getParameter("BOARD_CONTENT"));
+
+
+		BoardWriteProService boardWriteProService = new BoardWriteProService();
+		boolean isWriteSuccess = boardWriteProService.registArticle(boardBean);
+		System.out.println(isWriteSuccess);
+		if(!isWriteSuccess){
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('등록실패')");
+			out.println("history.back();");
+			out.println("</script>");
+		}
+		else{
+			forward = new ActionForward();
+			forward.setRedirect(true);
+			forward.setPath("boardList.bo");
+		}
+
+		return forward;
+	}
 }
--- dogpark_project/src/com/dogpark/dto/Dto.java
+++ dogpark_project/src/com/dogpark/dto/Dto.java
@@ -1,5 +1,26 @@
 package com.dogpark.dto;
 
 public class Dto {
-
+	String u_id;
+	String u_pw;
+	String u_email;
+	
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_pw() {
+		return u_pw;
+	}
+	public void setU_pw(String u_pw) {
+		this.u_pw = u_pw;
+	}
+	public String getU_email() {
+		return u_email;
+	}
+	public void setU_email(String u_email) {
+		this.u_email = u_email;
+	}
 }
