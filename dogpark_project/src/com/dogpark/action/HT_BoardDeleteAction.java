b4efb62217e20714ce427cb2687bdd79f888916f
Wonyoung Seo <swy1487@naver.com>
2020-07-31 14:39:28 +0900

    wy delete commit complete good

--- dogpark_project/src/com/dogpark/action/HT_BoardDeleteAction.java
+++ dogpark_project/src/com/dogpark/action/HT_BoardDeleteAction.java
@@ -1,10 +1,15 @@
 package com.dogpark.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dogpark.dto.ActionForward;
+import com.dogpark.dto.BoardBean;
 import com.dogpark.service.BoardDeleteService;
+import com.dogpark.service.BoardListService;
 import com.dogpark.service.BoardModifyReadService;
 
 public class HT_BoardDeleteAction implements Action{
@@ -15,7 +20,14 @@ public class HT_BoardDeleteAction implements Action{
 		BoardDeleteService boarddeleteService = new BoardDeleteService();
 		boarddeleteService.deleteService(code);
 		
-  		return null;
+		List<BoardBean> articleList=new ArrayList<BoardBean>();
+		BoardListService boardlistService = new BoardListService();
+		articleList=boardlistService.ListService();
+		request.setAttribute("articleList", articleList);
+		ActionForward forward= new ActionForward();
+  		forward.setPath("/HT_List.jsp");
+		
+  		return forward;
 	}
 
 }
