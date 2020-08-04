85c025b3481966b921491d912a8064543b1dc434
Administrator <Administrator@321-16>
2020-08-04 11:43:08 +0900

    Breeding Dto,mapper fix

---
+++ dogpark_project/src/com/dogpark/dto/BreedingDto.java
@@ -0,0 +1,55 @@
+package com.dogpark.dto;
+
+public class BreedingDto {
+	String u_id;
+	String nickname;
+	String phone_num;
+	String dog_name;
+	String dog_classify;
+	String dog_species;
+	String dbirth_year;
+	
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(String phone_num) {
+		this.phone_num = phone_num;
+	}
+	public String getDog_name() {
+		return dog_name;
+	}
+	public void setDog_name(String dog_name) {
+		this.dog_name = dog_name;
+	}
+	public String getDog_classify() {
+		return dog_classify;
+	}
+	public void setDog_classify(String dog_classify) {
+		this.dog_classify = dog_classify;
+	}
+	public String getDog_species() {
+		return dog_species;
+	}
+	public void setDog_species(String dog_species) {
+		this.dog_species = dog_species;
+	}
+	public String getDbirth_year() {
+		return dbirth_year;
+	}
+	public void setDbirth_year(String dbirth_year) {
+		this.dbirth_year = dbirth_year;
+	}
+	
+}
