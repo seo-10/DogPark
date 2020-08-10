60fe990e39514bfbb58fd838bde08d12f839ad98
Wonyoung Seo <swy1487@naver.com>
2020-08-10 17:41:03 +0900

    mapper u_id add wy

--- dogpark_project/src/com/dogpark/action/DS_ReservationAction.java
+++ dogpark_project/src/com/dogpark/action/DS_ReservationAction.java
@@ -11,15 +11,20 @@ public class DS_ReservationAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		String redate = request.getParameter("redate");
 		String retime = request.getParameter("retime");
+		String id= request.getParameter("id");
 		
 		CalendarDto dto = new CalendarDto();
 		dto.setDate(redate);
 		dto.setTime(retime);
+		dto.setU_id(id);
+		System.out.println(id);
 		DS_ReservationService ds_reservation = new DS_ReservationService();
 		ds_reservation.Reservationinsert(dto);
 		
 		
-		return null;
+		ActionForward forward= new ActionForward();
+		forward.setPath("dogpark_calendar.jsp");
+		return forward;
 		
 	}
 }
